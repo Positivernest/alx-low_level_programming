2177c1a5fc88eae406fedd50787ce3892b5dfde1
positivernest <adehjoh@gmail.com>
2022-10-13 14:35:47 -0700

    we put together

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,12 @@
 
 /**
  *
- *  main - Printing
+ * main - Printing
  *
- *   Return: Always 0 (Successful)
- *
- *    /
- *
- *    int main(void)
- *
- *    {
- *
- *    	puts("\"Programming is like building a multilingual puzzle");      
- *
- *    		return (0);
- *
- *    		}
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
