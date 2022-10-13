960bb2bad9db74a0bd7a50f181d7a310bac47a70
positivernest <adehjoh@gmail.com>
2022-10-13 12:45:09 -0700

    we put together

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Entry
- *
- *   *Return: Always 0
- *
- *    */
+ 
+   *main - Entry point
+ 
+    *Return: 0 after printing the function
+ 
+     */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");      
 
 			return (0);
 
