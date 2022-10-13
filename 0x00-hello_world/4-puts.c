d4faef77de49045936d700a96d271ea35546cb24
positivernest <adehjoh@gmail.com>
2022-10-13 14:18:20 -0700

    we put together

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ *  main - Printing
  *
- *   *
+ *   Return: Always 0 (Successful)
  *
- *    * Return: Always 0 (Success)
+ *    /
  *
- *     */
-
-int main(void)
-
-{
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
-}
+ *    int main(void)
+ *
+ *    {
+ *
+ *    	puts("\"Programming is like building a multilingual puzzle");      
+ *
+ *    		return (0);
+ *
+ *    		}
