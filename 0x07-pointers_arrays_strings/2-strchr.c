4d057f97a561d727f17ce49f3899ff540d52847b
positivernest <adehjoh@gmail.com>
2022-10-31 19:28:52 -0700

    strencher

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,25 +2,30 @@
 
 /**
  * _strchr - Loctes a character in a string
- *
- * @s: The string to check
- *
- * @c: The character to find
- *
- * Return: Pointer to @s or NULL
- *
- */
+ * @s: String
+ * @c: Character to search
+ * Return: Pointer to the first occurrence of the character c in the string s
+ **/
+
 char *_strchr(char *s, char c)
 {
-while (*s)
+
+while (*s != '\0') /*Declaring WHILE*/
 {
-s++;
+if (*s == c) /*if s ==  c*/
+{
+return (s); /*return s*/
+}
+
+++s;
+}
 
 if (*s == c)
 {
 return (s);
-}
+
 }
 
-return (s);
+
+return (0);/* values null*/
 }
