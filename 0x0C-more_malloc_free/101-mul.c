af357380dccb5e9a7c02fb58a6b07487914bc0ff
positivernest <adehjoh@gmail.com>
2022-11-10 12:19:00 -0800

    the mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,32 +1,77 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * main - multiply 2 numbers as arguments
- * @argc: number of arguments
- * @argv: arguments
- * Return: 0 success
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of wordsa
  */
-int main(int argc, char *argv[])
+int count_word(char *s)
 {
+int flag, c, w;
 
-unsigned long mul;
-int i, j;
-if (argc != 3)
-{ printf("Error\n");
-exit(98); }
-for (i = 1; i < argc; i++)
+flag = 0;
+w = 0;
+
+for (c = 0; s[c] != '\0'; c++)
 {
-for (j = 0; argv[i][j] != '\0'; j++)
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
 {
-if (argv[i][j] > 57 || argv[i][j] < 48)
-{  printf("Error\n");
-exit(98); }
+flag = 1;
+w++;
+}
 }
 
+return (w);
 }
-mul = atol(argv[1]) *  atol(argv[2]);
-printf("%lu\n", mul);
-return (0);
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
+
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
+return (NULL);
+
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return (NULL);
+
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (c++ == 0)
+start = i;
+}
+
+matrix[k] = NULL;
+
+return (matrix);
 }
