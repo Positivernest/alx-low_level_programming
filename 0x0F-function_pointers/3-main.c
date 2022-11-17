4230d1716dfd4f19d0e5146e72b3dcfbdc8955bc
positivernest <adehjoh@gmail.com>
2022-11-17 15:51:31 -0800

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,17 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
+#include <string.h>
 
 /**
- * main - Entry point
- *
- * @argc: length of command line arguments
- *
- * @argv: double pointer to cli arguments
- *
- * Return: 0
- */
+ * main - Prints result of an operation
+ * @argc: Number of arguments
+ * @argv: Value of arguments
+ * Return: Returns an int.
+**/
 
 int main(int argc, char *argv[])
 {
-int (*func_ptr)(int, int);
 
 if (argc != 4)
 {
@@ -22,21 +19,26 @@ printf("Error\n");
 exit(98);
 }
 
-if (argv[2][1] != '\0')
+if (
+(strcmp(argv[2], "+") &&
+strcmp(argv[2], "-") &&
+strcmp(argv[2], "*") &&
+strcmp(argv[2], "/") &&
+strcmp(argv[2], "%"))
+)
 {
 printf("Error\n");
 exit(99);
 }
 
-func_ptr = get_op_func(argv[2]);
-
-if (func_ptr == NULL)
+if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+&& atoi(argv[3]) == 0)
 {
 printf("Error\n");
-exit(99);
+exit(100);
 }
 
-printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
+printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
 
 return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,32 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h"
-
 /**
- * op_add - Adds two numbers @a and @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Sum of @a and @b
- */
-
+ * op_add - Adds two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
 int op_add(int a, int b)
 {
 return (a + b);
 }
 
 /**
- * op_sub - Calculates the difference of @c and @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Difference of @a and @b
- */
-
+ * op_sub - subtracts two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
 int op_sub(int a, int b)
 {
 return (a - b);
@@ -34,13 +22,10 @@ return (a - b);
 
 /**
  * op_mul - Multiplies two numbers
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Product of @a and @b
- */
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
 
 int op_mul(int a, int b)
 {
@@ -48,40 +33,25 @@ return (a * b);
 }
 
 /**
- * op_div - Divides @a by @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Result of the division of @a by @b without remainder
- */
+ * op_div - Divides two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
 
 int op_div(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
+return (a / b);
 }
 
 /**
- * op_mod - Calculates the remainder of @a divided by @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Remainder of @a divider by @b
- */
+ * op_mod - Modules two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
 
 int op_mod(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-
 return (a % b);
 }
